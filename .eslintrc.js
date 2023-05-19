8aff76110318673e95ca9fcaaa27ea2ee11e41e5
fazerog02 <fazerog02@gmail.com>
2023-05-19 17:03:04 +0900

    [add]base project

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    '@nuxtjs/eslint-config-typescript',
+    'plugin:nuxt/recommended',
+    'prettier',
+  ],
+  plugins: ['import', 'unused-imports'],
+  // add your custom rules here
+  rules: {
+    'import/order': [
+      'error',
+      {
+        alphabetize: {
+          order: 'asc',
+        },
+      },
+    ],
+  },
+};
