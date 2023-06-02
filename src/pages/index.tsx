05d817107d6ac5e1747004b9cc0034ef79c7db53
Ksw101010 <s2313950@u.tsukuba.ac.jp>
2023-06-02 17:03:45 +0900

    [add]画像を常に正方形に

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,29 +2,56 @@ import { Image, Stack } from "@chakra-ui/react";
 import { Grid, GridItem } from "@chakra-ui/react";
 import { AspectRatio } from "@chakra-ui/react";
 
-const Test2Page = () => {
+const IndexPage = () => {
   return (
-    <Grid templateColumns='repeat(4, 1fr)' gap={6}>
-      <GridItem w='100%' h='350' bg='blue.500'>
-        <Image boxSize='351px' objectFit='cover' src='/IMG_0649.png' alt='代替テキスト' />
+    <Grid templateColumns='repeat(4, 1fr)' gap={4}>
+      <GridItem bg='blue.500'>
+        <AspectRatio maxW='100%' ratio={1}>
+          <Image boxSize='100%' src='/IMG_0649.png' alt='代替テキスト' />
+        </AspectRatio>
       </GridItem>
-      <GridItem w='100%' h='350' bg='blue.500'>
-        <Image boxSize='351px' objectFit='cover' src='/AA.jpg' alt='代替テキスト' />
+      <GridItem bg='blue.500'>
+        <AspectRatio maxW='100%' ratio={1}>
+          <Image boxSize='100%' src='/AA.jpg' alt='代替テキスト' />
+        </AspectRatio>
       </GridItem>
-      <GridItem w='100%' h='350' bg='blue.500'>
-        <Image boxSize='351px' objectFit='cover' src='/AA2.jpg' alt='代替テキスト' />
+      <GridItem bg='blue.500'>
+        <AspectRatio maxW='100%' ratio={1}>
+          <Image boxSize='100%' src='/AA2.jpg' alt='代替テキスト' />
+        </AspectRatio>
       </GridItem>
-      <GridItem w='100%' h='350' bg='blue.500'>
-        <Image boxSize='351px' objectFit='cover' src='/R.jpg' alt='代替テキスト' />
+      <GridItem bg='blue.500'>
+        <AspectRatio maxW='100%' ratio={1}>
+          <Image boxSize='100%' src='/R.jpg' alt='代替テキスト' />
+        </AspectRatio>
       </GridItem>
-      <GridItem w='100%' h='350' bg='blue.500'>
-        <Image boxSize='351px' objectFit='cover' src='/SP.png' alt='代替テキスト' />
+      <GridItem bg='blue.500'>
+        <AspectRatio maxW='100%' ratio={1}>
+          <Image boxSize='100%' src='/SP.png' alt='代替テキスト' />
+        </AspectRatio>
       </GridItem>
-      <GridItem w='100%' h='350' bg='blue.500'>
-        <Image boxSize='351px' objectFit='cover' src='/IK.jpg' alt='代替テキスト' />
+      <GridItem bg='blue.500'>
+        <AspectRatio maxW='100%' ratio={1}>
+          <Image boxSize='100%' src='/IK.jpg' alt='代替テキスト' />
+        </AspectRatio>
       </GridItem>
+      <AspectRatio ratio={1}>
+        <GridItem bg='blue.500'>
+          <Image boxSize='100%' src='/OIP.jpg' alt='代替テキスト' />
+        </GridItem>
+      </AspectRatio>
+      <AspectRatio ratio={1}>
+        <GridItem bg='blue.500'>
+          <Image boxSize='100%' src='/BU.jpg' alt='代替テキスト' />
+        </GridItem>
+      </AspectRatio>
+      <AspectRatio ratio={1}>
+        <GridItem bg='blue.500'>
+          <Image boxSize='100%' src='/MA.jpg' alt='代替テキスト' />
+        </GridItem>
+      </AspectRatio>
     </Grid>
   );
 };
 
-export default Test2Page;
+export default IndexPage;
