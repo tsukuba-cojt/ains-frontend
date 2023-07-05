e9dc9705580609f3e24f5de5f42468b4de51f551
AbesKraken <tokatabea@gmail.com>
2023-07-05 12:24:06 +0900

    [add]LoginModalに実際にLoginを実装

--- src/components/LoginModal.tsx
+++ src/components/LoginModal.tsx
@@ -3,6 +3,7 @@ import { Button } from "@chakra-ui/react";
 import { VStack, Text, Box } from "@chakra-ui/react";
 import { Input, InputGroup, InputRightElement } from "@chakra-ui/react";
 import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton } from "@chakra-ui/react";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { useState } from "react";
 
 interface Props {
@@ -14,6 +15,11 @@ interface Props {
 const LoginModal = (props: Props) => {
   const [show, setShow] = useState(false);
   const switchPasswordVisible = () => setShow(!show);
+
+  const [email, setEmail] = useState("");
+  const [loginPass, setLoginPass] = useState("");
+  const auth = getAuth();
+
   return (
     <Modal isOpen={props.isOpen} onClose={props.onClose} isCentered blockScrollOnMount={false}>
       <ModalOverlay />
@@ -29,13 +35,23 @@ const LoginModal = (props: Props) => {
             <Box paddingBottom='30px'>
               <Text text-align='left'>メールアドレス</Text>
               <InputGroup size='md'>
-                <Input pr='4.5rem' type='email' placeholder='Enter email' />
+                <Input
+                  pr='4.5rem'
+                  type='email'
+                  placeholder='Enter email'
+                  onChange={(event) => setEmail(event.target.value)}
+                />
               </InputGroup>
             </Box>
             <Box paddingBottom='30px'>
               <Text>パスワード</Text>
               <InputGroup size='md'>
-                <Input pr='4.5rem' type={show ? "text" : "password"} placeholder='Enter password' />
+                <Input
+                  pr='4.5rem'
+                  type={show ? "text" : "password"}
+                  placeholder='Enter password'
+                  onChange={(event) => setLoginPass(event.target.value)}
+                />
                 <InputRightElement width='4.5rem'>
                   <Button h='1.75rem' size='sm' variant='ghost' onClick={switchPasswordVisible}>
                     {show ? <ViewIcon /> : <ViewOffIcon />}
@@ -44,7 +60,14 @@ const LoginModal = (props: Props) => {
               </InputGroup>
             </Box>
           </VStack>
-          <Button borderRadius='full' width='80%'>
+          <Button
+            borderRadius='full'
+            width='80%'
+            onClick={() => {
+              signInWithEmailAndPassword(auth, email, loginPass);
+              switchPasswordVisible();
+            }}
+          >
             ログイン
           </Button>
           <VStack align='left' w='350px' paddingBottom='30px'>
