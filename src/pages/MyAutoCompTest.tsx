68c29b411283daf96124c1a0c3b7414ccefc64ad
AbesKraken <tokatabea@gmail.com>
2024-01-05 11:19:36 +0900

    画像以外の作品のサムネイルを表示することに成功

--- src/pages/MyAutoCompTest.tsx
+++ src/pages/MyAutoCompTest.tsx
@@ -1,4 +1,4 @@
-import { CheckCircleIcon } from "@chakra-ui/icons";
+import { CheckCircleIcon, SpinnerIcon } from "@chakra-ui/icons";
 import { Input, Box, Flex, Text, Image } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import React from "react";
@@ -6,12 +6,51 @@ import { useState } from "react";
 import useSWR from "swr";
 
 import HoverTag from "@/components/HoverTag";
+import ThumbnailImage2 from "@/components/ThumbnailImage";
 import ArtworkInteractor from "@/interactors/Artwork/ArtworkInteractor";
 import { ArtworkData } from "@/interactors/Artwork/ArtworkTypes";
 
 export interface Item {
   label: string;
   value: string;
+}
+
+function ThumbnailImage(artworkData: ArtworkData) {
+  const {
+    data: imgURL,
+    error,
+    isLoading,
+  } = useSWR(`/thumbnail/${artworkData.id}`, async () => {
+    let thumbnail_url = "/text_file.png";
+    if (artworkData.type === "image") {
+      thumbnail_url = artworkData.file.url;
+    } else {
+      switch (artworkData.type) {
+        case "text": {
+          thumbnail_url = "/text_file.png";
+          break;
+        }
+        case "audio": {
+          thumbnail_url = "/audio_file.png";
+          break;
+        }
+        case "video": {
+          thumbnail_url = "/text_file.png";
+          break;
+        }
+      }
+      if (artworkData.parent_ids.length > 0) {
+        const parent_artwork = await new ArtworkInteractor().get(artworkData.parent_ids[0]);
+        if (parent_artwork !== null && parent_artwork.type === "image") thumbnail_url = parent_artwork.file.url;
+      }
+    }
+    return thumbnail_url;
+  });
+  if (imgURL !== null || imgURL !== undefined) {
+    return <Image src={imgURL} boxSize='20px' alt='image' marginRight='10px' marginLeft='10px' />;
+  } else {
+    return <SpinnerIcon boxSize='20px' marginRight='10px' marginLeft='10px' />;
+  }
 }
 
 export default function App() {
@@ -26,17 +65,20 @@ export default function App() {
     new ArtworkInteractor().fullTextSearch(10, serchBoxTexts.split(/\s+/))
   );
 
-  let artworksBox: JSX.Element = <>検索中…</>;
+  let artworksSelection: JSX.Element = <>検索中…</>;
   if (!error && artworks !== null && artworks !== undefined) {
+    //検索結果が取得できている
     if (artworks.length == 0 && serchBoxTexts.length > 0) {
-      artworksBox = <>検索結果なし</>;
+      //検索ワードが入力されているが、ヒットした作品がない。
+      artworksSelection = <>検索結果なし</>;
     } else {
-      artworksBox = (
+      artworksSelection = (
         <>
           {artworks.map((aData: ArtworkData) => {
             return (
               <>
-                <Box
+                <Flex
+                  align='center'
                   _hover={{ bg: "black" }}
                   onClick={() => {
                     console.log(`${aData.id}(${aData.name})`);
@@ -49,20 +91,14 @@ export default function App() {
                     }
                   }}
                 >
-                  <Flex align='center'>
-                    {selectedParentWorks.find((aAryData) => aAryData.value === aData.id) ? (
-                      <CheckCircleIcon boxSize='16px' />
-                    ) : (
-                      <Box boxSize='16px' />
-                    )}
-                    {aData.type === "image" ? (
-                      <Image src={aData.file.url} boxSize='20px' alt='image' marginRight='10px' marginLeft='10px' />
-                    ) : (
-                      <></>
-                    )}
-                    <Text textAlign={["left", "center"]}>{aData.name}</Text>
-                  </Flex>
-                </Box>
+                  {selectedParentWorks.find((aAryData) => aAryData.value === aData.id) ? (
+                    <CheckCircleIcon boxSize='16px' />
+                  ) : (
+                    <Box boxSize='16px' />
+                  )}
+                  <ThumbnailImage2 artworkData={aData} />
+                  <Text textAlign={["left", "center"]}>{aData.name}</Text>
+                </Flex>
               </>
             );
           })}
@@ -81,8 +117,7 @@ export default function App() {
             )
           }
         >
-          {" "}
-          {aData.label}{" "}
+          {aData.label}
         </HoverTag>
       </>
     );
@@ -92,9 +127,9 @@ export default function App() {
 
   return (
     <>
-      <Input variant='filled' placeholder='検索' onChange={(event) => setSerchBoxTexts(event.target.value)}></Input>
       <Box paddingLeft='20px' maxH='300px' overflowY='scroll'>
-        {artworksBox}
+        <Input variant='filled' placeholder='検索' onChange={(event) => setSerchBoxTexts(event.target.value)}></Input>
+        {artworksSelection}
       </Box>
       {selectedArtWorks}
     </>
